2d2c0dc468d334c9ba1f74efd6652f9047e5204a
Doef67 <74954400+Doef67@users.noreply.github.com>
2022-09-14 17:44:24 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter
+import nearest
+
+win = tkinter.Tk()
+win.geometry("500x600")
+
+tkinter.Label(win, text="Follow the points")
+
+
+points = [(2, 3), (-8, -9), (10, 11)]
+start = [(0,0)]
+#nearest = nearest_point(start, points)
+#print(nearest)
+points = start + points
+
+print(nearest.nearestBrute_route(points))
+
+win.mainloop()
