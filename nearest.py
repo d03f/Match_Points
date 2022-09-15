16db18a2b8aa40950c7bb34ed7fe687eb7bd8112
d03f <74954400+Doef67@users.noreply.github.com>
2022-09-15 11:25:53 +0200

    Update nearest.py
    Upload nearest Wave

--- nearest.py
+++ nearest.py
@@ -1,8 +1,37 @@
+import random
+
+
 def distance(x1, y1, x2, y2):
     x_dist = x1 - x2 if x1 - x2 > 0 else (x1 - x2) * -1
     y_dist = y1 - y2 if y1 - y2 > 0 else (y1 - y2) * -1
 
     return x_dist, y_dist
+
+def nearestWave(point, points, dist):
+    x1, y1 = point
+    nearPoints = []
+    distan = dist
+
+    while True:
+        for point in points:
+            nowX, nowY = point
+            x_bool = False; y_bool = False
+
+            if x1 - nowX < distan and x1 - nowX > -distan:
+                x_bool = True
+            if y1 - nowY < distan and y1 - nowY > -distan:
+                y_bool = True
+            
+            if x_bool and y_bool:
+                nearPoints.append(point)
+
+        if len(nearPoints) == 0: distan += dist
+        else:break
+    
+
+    return random.choice(nearPoints)
+
+        
 
 
 def nearestBrute_Point(start, points):
@@ -17,19 +46,48 @@ def nearestBrute_Point(start, points):
     return min_point
 
 def nearestBrute_route(points):
-    p_left = points
+    p_left = points.copy()
     order = [p_left.pop(0)]
     end = p_left.pop(-1)
+    
+    for i in range(len(points) - 1):
+        next = nearestBrute_Point(order[i], p_left)
+        try:p_left.remove(next)
+        except:break
+        order.append(next)
+
+    order.append(end)
+    return order
+
+
+def nearestBrute_short(points):
+    p_left = points.copy()
+    order = [p_left.pop(0)]
+    end = p_left[-1]
     
     for i in range(len(points)):
         next = nearestBrute_Point(order[i], p_left)
         p_left.remove(next)
         order.append(next)
+        if next == end:
+            break
+    
+    return order
 
-    order.append(end)
+def nearestWeave_short(points, dist):
+    p_left = points.copy()
+    order = [p_left.pop(0)]
+    end = p_left[-1]    
 
+    for i in range(len(points)):    
+        next = nearestWave(order[i], p_left, dist)
+        p_left.remove(next)    
+
+        order.append(next)
+        if next == end:
+            break
+    
     return order 
-
 
 def bruteForce(points):
     pass
@@ -39,3 +97,4 @@ def bruteForce(points):
 #start = [(0,0)]
 #route = nearestBrute_route(start + points)
 #print(route)
+
